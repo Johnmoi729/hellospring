18af9e29a04940ac2c4826815ba9ce89680aeca7
Johnmoi729 <superdogmeat13579@gmail.com>
2025-08-16 18:25:14 +0700

    first commit

---
+++ src/main/java/test/num/hellospring/HelloController.java
@@ -0,0 +1,19 @@
+package test.num.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
